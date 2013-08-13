6c9eb916c4e79abecbe8a0a3d64425d32edd6ba3
King Koopa <bbyholm@abo.fi>
2013-08-13 13:00:46 +0300

    Updated tests.

--- test/js/fromv8string-test.js
+++ test/js/fromv8string-test.js
@@ -10,7 +10,7 @@ test('fromv8string', function (t) {
         HEX    = 5,
         BUFFER = 6;
 
-  t.plan(8);
+  t.plan(15);
   t.type(bindings('fromv8string').r, 'function');
   t.equal(bindings('fromv8string').r('an ascii string', ASCII), 'an ascii string');
   t.equal(bindings('fromv8string').r('an utf8 strïng', UTF8), 'an utf8 strïng');
@@ -19,4 +19,11 @@ test('fromv8string', function (t) {
   t.equal(bindings('fromv8string').r(new Buffer('a binary string'), BINARY), 'a binary string');
   t.equal(bindings('fromv8string').r('612068657820737472696E67', HEX), 'a hex string');
   t.equal(bindings('fromv8string').r(new Buffer('a buffer string'), BUFFER), 'a buffer string');
+  t.equal(bindings('fromv8string').r('an ascii string', ASCII, 1), 'an ascii string');
+  t.equal(bindings('fromv8string').r('an utf8 strïng', UTF8, 1), 'an utf8 strïng');
+  t.equal(bindings('fromv8string').r('YSBiYXNlNjQgc3RyaW5n', BASE64, 1), 'a base64 string');
+  t.equal(bindings('fromv8string').r('an ucs2 strïng', UCS2, 1), 'an ucs2 strïng');
+  t.equal(bindings('fromv8string').r(new Buffer('a binary string'), BINARY, 1), 'a binary string');
+  t.equal(bindings('fromv8string').r('612068657820737472696E67', HEX, 1), 'a hex string');
+  t.equal(bindings('fromv8string').r(new Buffer('a buffer string'), BUFFER, 1), 'a buffer string');
 });
