e8d43c570892761703b44909c009d90f8c0be7b8
Rod Vagg <rod@vagg.org>
2013-11-24 15:06:59 +1100

    docker container test runner

---
+++ test/docker/run-docker.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+#############################################################
+# Run NAN tests in Docker containers for many Node versions #
+#############################################################
+
+__dirname="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# How many tests can we run at once without making the computer grind to a halt
+# or causing other unwanted resource problems:
+SIMULTANEOUS=4
+
+# The versions of Node to test, this assumes that we have a Docker image
+# set up with the name "node_dev-<version>"
+NODE_VERSIONS=`cat ${__dirname}/node_versions.list`
+
+START_TS=`date +%s`
+
+test_node() {
+  local OUT=/tmp/nan-test-${NV}.out
+  local NV=$1
+
+  # Run test in a Docker container
+  docker run -v ${__dirname}/../../:/nan-src/:ro node_dev-$NV /bin/bash -c " \
+    rsync -aAXx --delete /nan-src/ /nan/ --exclude .git --progress; \
+    cd /nan/test/; \
+    npm install; \
+    node_modules/.bin/node-gyp --nodedir /usr/src/node/ rebuild; \
+    node_modules/.bin/tap js/*-test.js; \
+  " &> $OUT
+
+  # Print status
+  printf "Node@\033[1m\033[33m%-8s\033[39m\033[22m: " $NV
+  if [[ `tail -1 /tmp/nan-test-${NV}.out` == "ok" ]]; then
+    echo -ne "\033[1m\033[32mPASS\033[39m\033[22m"
+  else
+    echo -ne "\033[1m\033[31mFAIL\033[39m\033[22m"
+  fi
+  echo -e " \033[3mwrote output to ${OUT}\033[23m"
+}
+
+# Run all tests
+_C=0
+for NV in $NODE_VERSIONS; do
+  test_node $NV &
+  # Small break between each start, gives Docker breathing room
+  sleep 0.5
+
+  # Crude limiting on the number of simultaneous runs
+  let _C=_C+1
+  if [[ $((_C % ${SIMULTANEOUS})) == 0 ]]; then
+    wait
+  fi
+done
+
+wait
+
+END_TS=`date +%s`
+DURATION=$((END_TS-START_TS))
+VERSIONS=$(echo $NODE_VERSIONS | wc -w)
+
+echo "Took ${DURATION}s to run ${VERSIONS} versions of Node"
---
+++ test/docker/setup-docker.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+##########################################################
+# Set up Docker containers with basic dev tools and Node #
+##########################################################
+
+__dirname="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# For `make -j X`
+BUILD_THREADS=6
+
+# The versions of Node to test, edit the node_versions.list file to change
+# NOTE: to re-make "master" just `sudo docker rmi node_dev-master` and run
+# this agai`sudo docker rmi node_dev-master` and run
+# this again
+NODE_VERSIONS=`cat ${__dirname}/node_versions.list`
+
+# Simple setup function for a container:
+#  setup_container(image id, base image, commands to run to set up)
+setup_container() {
+  local ID=$1
+  local BASE=$2
+  local RUN=$3
+
+  # Does this image exist? If yes, ignore
+  docker inspect "$ID" &> /dev/null
+  if [[ $? -eq 0 ]]; then
+    echo "Found existing container [$ID]"
+    return
+  fi
+
+  # No such image, so make it
+  echo "Did not find container container [$ID], creating..."
+  docker run $BASE /bin/bash -c "$RUN"
+  sleep 2
+  docker commit `docker ps -l -q` $ID
+}
+
+# A basic dev image with the build tools needed for Node
+setup_container "dev_base" "ubuntu" " \
+  apt-get update; \
+  apt-get install -y make gcc g++ python git"
+
+# The main Node repo in an image by itself
+setup_container "node_dev" "dev_base" " \
+  git clone https://github.com/joyent/node.git /usr/src/node/"
+
+# For each version of Node, make an image by checking out that branch
+# on the repo, building it and installing it
+for NV in $NODE_VERSIONS; do
+  setup_container "node_dev-$NV" "node_dev" " \
+    cd /usr/src/node; \
+    git checkout $NV; \
+    git pull origin $NV; \
+    ./configure; \
+    make -j${BUILD_THREADS}; \
+    make install"
+done
