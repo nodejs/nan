f30642ab0c12225d9d2a0a204385e7f28a45396f
Rod Vagg <rod@vagg.org>
2013-12-08 23:50:30 +1100

    added Nan*Persistent() tests

---
+++ test/cpp/persistent.cpp
@@ -0,0 +1,42 @@
+#include <nan.h>
+
+static v8::Persistent<v8::String> persistentTest1;
+
+NAN_METHOD(Save1) {
+  NanScope();
+
+  NanAssignPersistent(v8::String, persistentTest1, args[0].As<v8::String>());
+
+  NanReturnUndefined();
+}
+
+NAN_METHOD(Get1) {
+  NanScope();
+
+  NanReturnValue(NanPersistentToLocal<v8::String>(persistentTest1));
+}
+
+NAN_METHOD(Dispose1) {
+  NanScope();
+
+  NanDispose(persistentTest1);
+
+  NanReturnUndefined();
+}
+
+void Init (v8::Handle<v8::Object> target) {
+  target->Set(
+      NanSymbol("save1")
+    , v8::FunctionTemplate::New(Save1)->GetFunction()
+  );
+  target->Set(
+      NanSymbol("get1")
+    , v8::FunctionTemplate::New(Get1)->GetFunction()
+  );
+  target->Set(
+      NanSymbol("dispose1")
+    , v8::FunctionTemplate::New(Dispose1)->GetFunction()
+  );
+}
+
+NODE_MODULE(persistent, Init)
