09fec78ff6999b6f99c129eeeb6c2639cb85451d
Mathias <mathiask@hotmail.de>
2014-06-19 09:11:55 +0200

    fixed indentation

--- nan.h
+++ nan.h
@@ -680,11 +680,11 @@ NAN_INLINE uint32_t NanUInt32OptionValue(
   }
 
   NAN_INLINE void NanSetTemplate(
-	  v8::Handle<v8::Template> templ
-	  , v8::Handle<v8::String> name
-	  , v8::Handle<v8::Data> value
-	  , v8::PropertyAttribute attributes) {
-	  templ->Set(name, value, attributes);
+      v8::Handle<v8::Template> templ
+    , v8::Handle<v8::String> name
+    , v8::Handle<v8::Data> value
+    , v8::PropertyAttribute attributes) {
+    templ->Set(name, value, attributes);
   }
 
   NAN_INLINE v8::Local<v8::Context> NanGetCurrentContext() {
@@ -1351,11 +1351,11 @@ NAN_INLINE _NanWeakCallbackInfo<T, P>* NanMakeWeakPersistent(
   }
 
   NAN_INLINE void NanSetTemplate(
-	  v8::Handle<v8::Template> templ
-	  , v8::Handle<v8::String> name
-	  , v8::Handle<v8::Data> value
-	  , v8::PropertyAttribute attributes) {
-	  templ->Set(name, value, attributes);
+      v8::Handle<v8::Template> templ
+    , v8::Handle<v8::String> name
+    , v8::Handle<v8::Data> value
+    , v8::PropertyAttribute attributes) {
+    templ->Set(name, value, attributes);
   }
 
   NAN_INLINE v8::Local<v8::Context> NanGetCurrentContext() {
@@ -2234,33 +2234,33 @@ NAN_INLINE void NanSetPrototypeTemplate(
   , const char *name
   , v8::Handle<v8::Data> value
 ) {
-    NanSetTemplate(templ->PrototypeTemplate(), name, value);
+  NanSetTemplate(templ->PrototypeTemplate(), name, value);
 }
 
 NAN_INLINE void NanSetPrototypeTemplate(
-	v8::Local<v8::FunctionTemplate> templ
-	, v8::Handle<v8::String> name
-	, v8::Handle<v8::Data> value
-	, v8::PropertyAttribute attributes
-	) {
-	NanSetTemplate(templ->PrototypeTemplate(), name, value, attributes);
+    v8::Local<v8::FunctionTemplate> templ
+  , v8::Handle<v8::String> name
+  , v8::Handle<v8::Data> value
+  , v8::PropertyAttribute attributes
+) {
+  NanSetTemplate(templ->PrototypeTemplate(), name, value, attributes);
 }
 
 NAN_INLINE void NanSetInstanceTemplate(
-	v8::Local<v8::FunctionTemplate> templ
-	, const char *name
-	, v8::Handle<v8::Data> value
-	) {
-	NanSetTemplate(templ->InstanceTemplate(), name, value);
+    v8::Local<v8::FunctionTemplate> templ
+  , const char *name
+  , v8::Handle<v8::Data> value
+) {
+  NanSetTemplate(templ->InstanceTemplate(), name, value);
 }
 
 NAN_INLINE void NanSetInstanceTemplate(
-	v8::Local<v8::FunctionTemplate> templ
-	, v8::Handle<v8::String> name
-	, v8::Handle<v8::Data> value
-	, v8::PropertyAttribute attributes
-	) {
-	NanSetTemplate(templ->InstanceTemplate(), name, value, attributes);
+    v8::Local<v8::FunctionTemplate> templ
+  , v8::Handle<v8::String> name
+  , v8::Handle<v8::Data> value
+  , v8::PropertyAttribute attributes
+) {
+  NanSetTemplate(templ->InstanceTemplate(), name, value, attributes);
 }
 
 #endif  // NAN_H_
