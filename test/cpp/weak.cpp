8630ce58e73af8afee2d4b6eb0ee1bd84e1a07d7
David Siegel <david.siegel@artcom.de>
2014-11-08 21:14:28 +0100

    WeakPersistent redesign

--- test/cpp/weak.cpp
+++ test/cpp/weak.cpp
@@ -8,7 +8,8 @@
 
 #include <nan.h>
 
-NAN_WEAK_CALLBACK(weakCallback) {
+void weakCallback(
+NanWeakCallbackData<v8::Function, int> & data) {  // NOLINT(runtime/references)
   int *parameter = data.GetParameter();
   NanMakeCallback(NanGetCurrentContext()->Global(), data.GetValue(), 0, NULL);
   if ((*parameter)++ == 0) {
@@ -19,10 +20,11 @@ NAN_WEAK_CALLBACK(weakCallback) {
 }
 
 v8::Handle<v8::String> wrap(v8::Local<v8::Function> func) {
+  NanEscapableScope();
   v8::Local<v8::String> lstring = NanNew<v8::String>("result");
   int *parameter = new int(0);
-  NanMakeWeakPersistent(func, parameter, &weakCallback);
-  return lstring;
+  NanMakeWeakPersistent(func, parameter, weakCallback);
+  return NanEscapeScope(lstring);
 }
 
 NAN_METHOD(Hustle) {
