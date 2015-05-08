d2c056b5e891703ad4ca946151743a8a0dee1ea1
King Koopa <bbyholm@abo.fi>
2015-05-08 22:11:31 +0300

    Remove remnants of 0.6 compatibility

--- nan.h
+++ nan.h
@@ -1231,16 +1231,7 @@ NAN_INLINE _NanWeakCallbackInfo<T, P>* NanMakeWeakPersistent(
     , v8::Handle<v8::Function> func
     , int argc
     , v8::Handle<v8::Value>* argv) {
-# if NODE_VERSION_AT_LEAST(0, 8, 0)
     return NanNew(node::MakeCallback(target, func, argc, argv));
-# else
-    v8::TryCatch try_catch;
-    v8::Local<v8::Value> result = func->Call(target, argc, argv);
-    if (try_catch.HasCaught()) {
-        node::FatalException(try_catch);
-    }
-    return result;
-# endif
   }
 
   NAN_INLINE v8::Local<v8::Value> NanMakeCallback(
@@ -1248,12 +1239,7 @@ NAN_INLINE _NanWeakCallbackInfo<T, P>* NanMakeWeakPersistent(
     , v8::Handle<v8::String> symbol
     , int argc
     , v8::Handle<v8::Value>* argv) {
-# if NODE_VERSION_AT_LEAST(0, 8, 0)
     return NanNew(node::MakeCallback(target, symbol, argc, argv));
-# else
-    v8::Local<v8::Function> callback = target->Get(symbol).As<v8::Function>();
-    return NanMakeCallback(target, callback, argc, argv);
-# endif
   }
 
   NAN_INLINE v8::Local<v8::Value> NanMakeCallback(
@@ -1261,11 +1247,7 @@ NAN_INLINE _NanWeakCallbackInfo<T, P>* NanMakeWeakPersistent(
     , const char* method
     , int argc
     , v8::Handle<v8::Value>* argv) {
-# if NODE_VERSION_AT_LEAST(0, 8, 0)
     return NanNew(node::MakeCallback(target, method, argc, argv));
-# else
-    return NanMakeCallback(target, NanNew(method), argc, argv);
-# endif
   }
 
   NAN_INLINE void NanFatalException(const v8::TryCatch& try_catch) {
@@ -1515,7 +1497,6 @@ class NanCallback {
                            , v8::Handle<v8::Value> argv[]) const {
     NanEscapableScope();
 
-#if NODE_VERSION_AT_LEAST(0, 8, 0)
     v8::Local<v8::Function> callback = handle->
         Get(kCallbackIndex).As<v8::Function>();
     return NanEscapeScope(node::MakeCallback(
@@ -1524,12 +1505,6 @@ class NanCallback {
       , argc
       , argv
     ));
-#else
-    v8::Local<v8::Function> callback = handle->
-        Get(kCallbackIndex).As<v8::Function>();
-    return NanEscapeScope(NanMakeCallback(
-        target, callback, argc, argv));
-#endif
   }
 #endif
 };
