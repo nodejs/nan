74087429526f585b111f485920c246758d52c478
Benjamin Byholm <bbyholm@abo.fi>
2015-05-13 16:17:56 +0300

    Removed String converters

--- test/cpp/multifile2.cpp
+++ test/cpp/multifile2.cpp
@@ -9,6 +9,6 @@
 #include <nan.h>
 
 NAN_METHOD(ReturnString) {
-  v8::Local<v8::String> s = NanNew<v8::String>(*NanUtf8String(args[0]));
+  v8::Local<v8::String> s = NanNew<v8::String>(*v8::String::Utf8Value(args[0]));
   NanReturnValue(s);
 }
