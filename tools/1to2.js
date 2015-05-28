380f1d8f07b01d3576d24cbd1c696a52d42fdc53
Benjamin Byholm <bbyholm@abo.fi>
2015-05-28 14:58:47 +0300

    Additional fixes and additions

--- tools/1to2.js
+++ tools/1to2.js
@@ -7,41 +7,51 @@
  ********************************************************************/
 
 var events = require('events'),
-    emitter = new events.EventEmitter(),
     fs = require('fs'),
     glob = require('glob'),
-    i, length, removedregex = [];
-
-const removed = [
-  'GetIndexedPropertiesExternalArrayDataLength',
-  'GetIndexedPropertiesExternalArrayData',
-  'GetIndexedPropertiesExternalArrayDataType',
-  'GetIndexedPropertiesPixelData',
-  'GetIndexedPropertiesPixelDataLength',
-  'HasIndexedPropertiesInExternalArrayData',
-  'HasIndexedPropertiesInPixelData',
-  'SetIndexedPropertiesToExternalArrayData',
-  'SetIndexedPropertiesToPixelData'];
-
-length = removed.length;
-
-for (i = 2; i < process.argv.length; i++) {
-  glob(process.argv[i], function (err, matches) {
-    if (err) {
-      throw err;
-    }
-    matches.forEach(function (currentValue) {
-      emitter.emit('file', currentValue);
-    });
-  });
-}
-
-for (i = 0; i < length; i++) {
-  removedregex[i] = new RegExp('^(.*)\\W' + removed[i] + '\\W(.*)', 'm');
-}
+    removed = [
+      'GetIndexedPropertiesExternalArrayDataLength',
+      'GetIndexedPropertiesExternalArrayData',
+      'GetIndexedPropertiesExternalArrayDataType',
+      'GetIndexedPropertiesPixelData',
+      'GetIndexedPropertiesPixelDataLength',
+      'HasIndexedPropertiesInExternalArrayData',
+      'HasIndexedPropertiesInPixelData',
+      'SetIndexedPropertiesToExternalArrayData',
+      'SetIndexedPropertiesToPixelData'],
+    removedregex = [],
+    callbacks = [
+      'NAN_METHOD',
+      'NAN_GETTER',
+      'NAN_SETTER',
+      'NAN_PROPERTY_GETTER',
+      'NAN_PROPERTY_SETTER',
+      'NAN_PROPERTY_ENUMERATOR',
+      'NAN_PROPERTY_DELETER',
+      'NAN_PROPERTY_QUERY',
+      'NAN_INDEX_GETTER',
+      'NAN_INDEX_SETTER',
+      'NAN_INDEX_ENUMERATOR',
+      'NAN_INDEX_DELETER',
+      'NAN_INDEX_QUERY'],
+    callbackregex = [],
+    length,
+    i;
 
 function replace(file, s) {
-  var i;
+  var i, length;
+
+  for (i = 0, length = removedregex.length; i < length; i++) {
+    s = s.replace(removedregex[i], function (match) {
+    return '// ERROR: Rewrite using Buffer\n' +  match;
+    });
+  }
+
+  for (i = 0, length = callbackregex.length; i < length; i++) {
+    s = s.replace(callbackregex[i], function (match, p1) {
+    return p1;
+    });
+  }
 
   s = s.replace(/(\W)args(\W)/g, function (match, p1, p2) {
     return [p1, 'info', p2].join('');
@@ -51,26 +61,45 @@ function replace(file, s) {
 
   s = s.replace(/NanEscapableScope\(\)/g, 'NanEscapableScope scope');
 
+  s = s.replace(/NanEscapeScope/g, 'scope.Escape');
+
   s = s.replace(/NanReturnValue/g, 'info.GetReturnValue().Set');
 
   s = s.replace(/NanReturnUndefined\(\)/g, 'return');
 
   s = s.replace(/NanReturnNull/g, 'info.GetReturnValue().SetNull');
 
+  s = s.replace(/NanAssignPersistent(?:<v8\:\:.*>)?\((.*),\s?(.*)\)/g,
+      function(match, p1, p2){
+    return [p1, '.Reset(', p2, ')'].join('');
+  })
+
   s = s.replace(/NanDisposePersistent\s*\(\s*(\w+)/g, function(match, p1) {
     return p1 + '.Reset(';
   });
 
-  s = s.replace(/(?:v8\:\:)?Persistent/g, function() { return 'NanPersistent'; });
+  s = s.replace(/(\W)(?:v8\:\:)?Persistent(\W)/g, function(match, p1, p2) {
+    return [p1, 'NanPersistent', p2].join(''); });
 
   s = s.replace(/NanSetWeak\s*\(\s*(\w+)\s*,/g, function(match, p1) {
     return p1 + '.SetWeak(';
   });
 
-  s = s.replace(/(?:node\:\:)?ObjectWrap/g, function() { return 'NanObjectWrap'; });
-
   s = s.replace(/NanObjectWrapHandle\s*\((\w+)/g, function (match, p1) {
-    return p1 + '.handle(';
+    return p1 + '->handle(';
+  });
+
+  s = s.replace(/(\W)(?:node\:\:)?ObjectWrap(\W)/g, function(match, p1, p2) {
+    return [p1, 'NanObjectWrap', p2].join('');
+  });
+
+  s = s.replace(/(\w+)->Set\(/g, function (match, p1) {
+    return 'NanSet(' + p1 + ', '
+  });
+
+  s = s.replace(/NAN_WEAK_CALLBACK\(([^\)]+)\)/, function (match, p1) {
+    return ['template<typename T>\nvoid ',
+        p1, '(const NanWeakCallbackInfo<T> &data)'].join('');
   });
 
   s = s.replace(/NODE_SET_METHOD/g, 'NanSetMethod');
@@ -84,12 +113,6 @@ function replace(file, s) {
   s = s.replace(/NanUcs2String/g, 'v8::String::Value');
 
   s = s.replace(/_NAN_/g, 'NAN_');
-
-  for (i = 0; i < length; i++) {
-    s = s.replace(removedregex[i], function (match, p1, p2) {
-    return '// ERROR: Rewrite using Buffer\n' +  match;
-    });
-  }
 
   fs.writeFile(file, s, function (err) {
     if (err) {
@@ -108,4 +131,22 @@ function processFile(file) {
   });
 }
 
-emitter.on('file', processFile);
+for (i = 0, length = removed.length; i < length; i++) {
+  removedregex[i] = new RegExp('^(.*)\\W' + removed[i] + '\\W(.*)', 'm');
+}
+
+for (i = 0, length = callbacks.length; i < length; i++) {
+  callbackregex[i] =
+      new RegExp('(' + callbacks[i] + '\\(.*\\)\\s*\\{)\\s*NanScope\\(\\)\\s*;', 'g');
+}
+
+for (i = 2, length = process.argv.length; i < length; i++) {
+  glob(process.argv[i], function (err, matches) {
+    if (err) {
+      throw err;
+    }
+    matches.forEach(function (currentValue) {
+      processFile(currentValue);
+    });
+  });
+}
