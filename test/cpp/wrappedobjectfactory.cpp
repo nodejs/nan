bff133ab771eef56315bb2f498a0d95777246f1f
Christopher J. Brody <brodybits@litehelpers.net>
2015-10-05 16:01:25 +0200

    test: Add inner object function to wrappedobjectfactory test
    Also keep Persistent<v8::Function> constructor in an inline function

--- test/cpp/wrappedobjectfactory.cpp
+++ test/cpp/wrappedobjectfactory.cpp
@@ -11,7 +11,8 @@
 using namespace Nan;  // NOLINT(build/namespaces)
 
 // ref: https://nodejs.org/api/addons.html#addons_factory_of_wrapped_objects
-class MyObject : public ObjectWrap {
+
+class InnerObject : public ObjectWrap {
  public:
   static NAN_MODULE_INIT(Init) {
     v8::Local<v8::FunctionTemplate> tpl = Nan::New<v8::FunctionTemplate>(New);
@@ -19,11 +20,59 @@ class MyObject : public ObjectWrap {
 
     SetPrototypeMethod(tpl, "getValue", GetValue);
 
-    constructor.Reset(GetFunction(tpl).ToLocalChecked());
+    constructor().Reset(GetFunction(tpl).ToLocalChecked());
+  }
+
+  static v8::Local<v8::Object> NewInstance(int argc, v8::Local<v8::Value> argv[]) {
+    v8::Local<v8::Function> cons = Nan::New(constructor());
+    return Nan::NewInstance(cons, argc, argv).ToLocalChecked();
+  }
+
+ private:
+  explicit InnerObject(double value = 0) : value_(value) {}
+  ~InnerObject() {}
+
+  static NAN_METHOD(New) {
+    if (info.IsConstructCall()) {
+      double value = info[0]->IsNumber() ? To<double>(info[0]).FromJust() : 0;
+      InnerObject * obj = new InnerObject(value);
+      obj->Wrap(info.This());
+      info.GetReturnValue().Set(info.This());
+    } else {
+      const int argc = 1;
+      v8::Local<v8::Value> argv[argc] = {info[0]};
+      v8::Local<v8::Function> cons = Nan::New(constructor());
+      info.GetReturnValue().Set(Nan::NewInstance(cons, argc, argv).ToLocalChecked());
+    }
+  }
+
+  static NAN_METHOD(GetValue) {
+    InnerObject* obj = ObjectWrap::Unwrap<InnerObject>(info.This());
+    info.GetReturnValue().Set(obj->value_);
+  }
+
+  static inline Persistent<v8::Function> & constructor() {
+    static Persistent<v8::Function> my_constructor;
+    return my_constructor;
+  }
+
+  double value_;
+};
+
+class MyObject : public ObjectWrap {
+ public:
+  static NAN_MODULE_INIT(Init) {
+    v8::Local<v8::FunctionTemplate> tpl = Nan::New<v8::FunctionTemplate>(New);
+    tpl->InstanceTemplate()->SetInternalFieldCount(1);
+
+    SetPrototypeMethod(tpl, "getValue", GetValue);
+    SetPrototypeMethod(tpl, "newInnerObject", NewInnerObject);
+
+    constructor().Reset(GetFunction(tpl).ToLocalChecked());
   }
 
   static NAN_METHOD(NewInstance) {
-    v8::Local<v8::Function> cons = Nan::New(constructor);
+    v8::Local<v8::Function> cons = Nan::New(constructor());
     double value = info[0]->IsNumber() ? To<double>(info[0]).FromJust() : 0;
     const int argc = 1;
     v8::Local<v8::Value> argv[1] = {Nan::New(value)};
@@ -43,7 +92,7 @@ class MyObject : public ObjectWrap {
     } else {
       const int argc = 1;
       v8::Local<v8::Value> argv[argc] = {info[0]};
-      v8::Local<v8::Function> cons = Nan::New(constructor);
+      v8::Local<v8::Function> cons = Nan::New(constructor());
       info.GetReturnValue().Set(Nan::NewInstance(cons, argc, argv).ToLocalChecked());
     }
   }
@@ -53,13 +102,22 @@ class MyObject : public ObjectWrap {
     info.GetReturnValue().Set(obj->value_);
   }
 
-  static Persistent<v8::Function> constructor;
+  static NAN_METHOD(NewInnerObject) {
+    const int argc = 1;
+    v8::Local<v8::Value> argv[argc] = {info[0]};
+    info.GetReturnValue().Set(InnerObject::NewInstance(argc, argv));
+  }
+
+  static inline Persistent<v8::Function> & constructor() {
+    static Persistent<v8::Function> my_constructor;
+    return my_constructor;
+  }
+
   double value_;
 };
 
-Persistent<v8::Function> MyObject::constructor;
-
 NAN_MODULE_INIT(Init) {
+  InnerObject::Init(target);
   MyObject::Init(target);
   Set(target
     , New<v8::String>("newFactoryObjectInstance").ToLocalChecked()
--- test/js/wrappedobjectfactory-test.js
+++ test/js/wrappedobjectfactory-test.js
@@ -10,12 +10,15 @@ const test     = require('tap').test
     , testRoot = require('path').resolve(__dirname, '..')
     , bindings = require('bindings')({ module_root: testRoot, bindings: 'wrappedobjectfactory' });
 
-test('objectwraphandle', function (t) {
-  t.plan(3);
+test('wrappedobjectfactory', function (t) {
+  t.plan(4);
 
   var obj = bindings.newFactoryObjectInstance(123);
 
   t.type(obj.getValue, 'function');
   t.type(obj.getValue(), 'number');
   t.equal(obj.getValue(), 123);
+
+  var inner = obj.newInnerObject(456);
+  t.equal(inner.getValue(), 456);
 });
