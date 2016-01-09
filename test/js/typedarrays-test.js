4aadec4f674f1d9872188296f228c1eec63803e1
Benjamin Byholm <bbyholm@abo.fi>
2016-01-09 16:50:38 +0200

    Update copyright year to 2016

--- test/js/typedarrays-test.js
+++ test/js/typedarrays-test.js
@@ -1,3 +1,11 @@
+/*********************************************************************
+ * NAN - Native Abstractions for Node.js
+ *
+ * Copyright (c) 2016 NAN contributors
+ *
+ * MIT License <https://github.com/nodejs/nan/blob/master/LICENSE.md>
+ ********************************************************************/
+
 const test     = require('tap').test
     , testRoot = require('path').resolve(__dirname, '..')
     , bindings = require('bindings')({ module_root: testRoot, bindings: 'typedarrays' });
