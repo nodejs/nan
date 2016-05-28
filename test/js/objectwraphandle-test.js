d73b667f9c0729bf2f93390324fed68df3ae26b0
Benjamin Byholm <bbyholm@abo.fi>
2016-05-28 18:41:09 +0300

    Make ObjectWrap::handle() const

--- test/js/objectwraphandle-test.js
+++ test/js/objectwraphandle-test.js
@@ -11,15 +11,17 @@ const test     = require('tap').test
     , bindings = require('bindings')({ module_root: testRoot, bindings: 'objectwraphandle' });
 
 test('objectwraphandle', function (t) {
-  t.plan(7);
+  t.plan(9);
 
   t.type(bindings.MyObject, 'function');
 
   var obj = new bindings.MyObject(10);
 
   t.type(obj.getHandle, 'function');
+  t.type(obj.getHandleConst, 'function');
   t.type(obj.getValue, 'function');
   t.type(obj.getHandle(), 'object');
+  t.type(obj.getHandleConst(), 'object');
   t.type(obj.getValue(), 'number');
 
   var derived = Object.create(obj);
