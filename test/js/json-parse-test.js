a71d2e1355fa8dde1c69a7f077cff3288dac789d
Michael Ira Krufky <mkrufky@gmail.com>
2017-03-17 07:43:21 -0400

    test/js/json-parse-test.js: add more test cases
    Signed-off-by: Michael Ira Krufky <mkrufky@gmail.com>

--- test/js/json-parse-test.js
+++ test/js/json-parse-test.js
@@ -3,10 +3,34 @@ const test     = require('tap').test
     , bindings = require('bindings')({ module_root: testRoot, bindings: 'parse' });
 
 test('parse', function (t) {
-  t.plan(2);
+  t.plan(8);
   t.type(bindings.parse, 'function');
   t.deepEqual(
     bindings.parse('{ "a": "JSON", "string": "value" }'),
     JSON.parse('{ "a": "JSON", "string": "value" }')
+  );
+  t.deepEqual(
+    bindings.parse('[1, 2, 3]'),
+    JSON.parse('[1, 2, 3]')
+  );
+  t.equal(
+    bindings.parse('57'),
+    JSON.parse('57')
+  );
+  t.equal(
+    bindings.parse('3.14159'),
+    JSON.parse('3.14159')
+  );
+  t.equal(
+    bindings.parse('true'),
+    JSON.parse('true')
+  );
+  t.equal(
+    bindings.parse('false'),
+    JSON.parse('false')
+  );
+  t.equal(
+    bindings.parse('"some string"'),
+    JSON.parse('"some string"')
   );
 });
