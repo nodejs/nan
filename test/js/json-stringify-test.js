c7edbc61962c9dfe9242b6e865d8bf70dcc712fa
Michael Ira Krufky <mkrufky@gmail.com>
2017-03-17 07:52:01 -0400

    test/js/json-stringify-test.js: add more test cases
    Signed-off-by: Michael Ira Krufky <mkrufky@gmail.com>

--- test/js/json-stringify-test.js
+++ test/js/json-stringify-test.js
@@ -3,7 +3,7 @@ const test     = require('tap').test
     , bindings = require('bindings')({ module_root: testRoot, bindings: 'stringify' });
 
 test('stringify', function (t) {
-  t.plan(4);
+  t.plan(22);
   t.type(bindings.stringify, 'function');
   t.equal(
     bindings.stringify({ "a": "JSON", "object": "value" }),
@@ -16,5 +16,77 @@ test('stringify', function (t) {
   t.equal(
     bindings.stringify({ "a": "JSON", "object": "value" }, null, '++'),
     JSON.stringify({ "a": "JSON", "object": "value" }, null, '++')
+  );
+  t.equal(
+    bindings.stringify([ 1, 2, 3 ]),
+    JSON.stringify([ 1, 2, 3 ])
+  );
+  t.equal(
+    bindings.stringify([ 1, 2, 3 ], null, 2),
+    JSON.stringify([ 1, 2, 3 ], null, 2)
+  );
+  t.equal(
+    bindings.stringify([ 1, 2, 3 ], null, '++'),
+    JSON.stringify([ 1, 2, 3 ], null, '++')
+  );
+  t.equal(
+    bindings.stringify("a string"),
+    JSON.stringify("a string")
+  );
+  t.equal(
+    bindings.stringify("a string", null, 2),
+    JSON.stringify("a string", null, 2)
+  );
+  t.equal(
+    bindings.stringify("a string", null, '++'),
+    JSON.stringify("a string", null, '++')
+  );
+  t.equal(
+    bindings.stringify(3.14159),
+    JSON.stringify(3.14159)
+  );
+  t.equal(
+    bindings.stringify(3.14159, null, 2),
+    JSON.stringify(3.14159, null, 2)
+  );
+  t.equal(
+    bindings.stringify(3.14159, null, '++'),
+    JSON.stringify(3.14159, null, '++')
+  );
+  t.equal(
+    bindings.stringify(-31),
+    JSON.stringify(-31)
+  );
+  t.equal(
+    bindings.stringify(-31, null, 2),
+    JSON.stringify(-31, null, 2)
+  );
+  t.equal(
+    bindings.stringify(-31, null, '++'),
+    JSON.stringify(-31, null, '++')
+  );
+  t.equal(
+    bindings.stringify(true),
+    JSON.stringify(true)
+  );
+  t.equal(
+    bindings.stringify(true, null, 2),
+    JSON.stringify(true, null, 2)
+  );
+  t.equal(
+    bindings.stringify(true, null, '++'),
+    JSON.stringify(true, null, '++')
+  );
+  t.equal(
+    bindings.stringify(false),
+    JSON.stringify(false)
+  );
+  t.equal(
+    bindings.stringify(false, null, 2),
+    JSON.stringify(false, null, 2)
+  );
+  t.equal(
+    bindings.stringify(false, null, '++'),
+    JSON.stringify(false, null, '++')
   );
 });
