66a14aa00cd4f472584f329f3912d0dc38b29c52
Michael Ira Krufky <mkrufky@gmail.com>
2017-09-03 15:45:08 -0400

    add test case to demonstrate that asyncprogressworker can miss events

--- test/js/asyncprogressworker-1ms-test.js
+++ test/js/asyncprogressworker-1ms-test.js
@@ -10,15 +10,15 @@ const test     = require('tap').test
     , testRoot = require('path').resolve(__dirname, '..')
     , bindings = require('bindings')({ module_root: testRoot, bindings: 'asyncprogressworker' });
 
-test('asyncprogressworker', function (t) {
-  // test with 100 ms sleep
+test('asyncprogressworker1ms', function (t) {
+  // test with 1 ms sleep
   var worker = bindings.a
     , progressed = 0
-  worker(100, 5, function(i) {
-    t.ok(i === progressed, 'got the progress updates #' + i);
+  worker(1, 500, function(i) {
+    t.ok(i >= progressed, 'got the progress updates #' + i);
     progressed++;
   }, function () {
-    t.ok(progressed === 5, 'got all progress updates')
+    t.ok(progressed <= 500, 'got some but not necessarily all progress updates')
     t.end()
   })
 })
