c997c88f4fb1c446c71d2bfb5795119c56c63d8e
Ryan Petrich <rpetrich@gmail.com>
2018-06-24 13:39:41 -0400

    Adjust formatting of tap-as-worker to be two spaced indentation and single quotes

--- test/tap-as-worker.js
+++ test/tap-as-worker.js
@@ -1,12 +1,12 @@
 // Run the tap tests in a worker
 // Not a clean approach, but is sufficient to verify correctness
-const worker_threads = require("worker_threads");
-const path = require("path");
+const worker_threads = require('worker_threads');
+const path = require('path');
 
 if (worker_threads.isMainThread) {
-	const worker = new worker_threads.Worker(__filename, { workerData: process.argv });
-	worker.on("error", console.error);
+  const worker = new worker_threads.Worker(__filename, { workerData: process.argv });
+  worker.on('error', console.error);
 } else {
-	process.argv = worker_threads.workerData;
-	require(path.resolve(require.resolve("tap"), "../../bin/tap.js"));
+  process.argv = worker_threads.workerData;
+  require(path.resolve(require.resolve('tap'), '../../bin/tap.js'));
 }
