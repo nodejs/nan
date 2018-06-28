b64de7612d1c07be111030f10dfe4e27ee28678f
Ryan Petrich <rpetrich@gmail.com>
2018-06-28 09:45:09 -0400

    Adjust tap-as-worker to properly return non-zero status code when test running in worker fails

--- test/tap-as-worker.js
+++ test/tap-as-worker.js
@@ -4,8 +4,13 @@ const worker_threads = require('worker_threads');
 const path = require('path');
 
 if (worker_threads.isMainThread) {
-  const worker = new worker_threads.Worker(__filename, { workerData: process.argv });
-  worker.on('error', console.error);
+  const worker = new worker_threads.Worker(__filename, {
+    workerData: process.argv
+  });
+  worker.on('error', () => {
+    // Worker will have logged the error, but will not set the exit code
+    process.exit(1);
+  });
 } else {
   process.argv = worker_threads.workerData;
   require(path.resolve(require.resolve('tap'), '../../bin/tap.js'));
