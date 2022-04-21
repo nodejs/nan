4be3e5c4e93ddf2014b71ad9717078d76c9ed549
Momtchil Momtchev <momtchil@momtchev.com>
2022-04-21 20:33:50 +0200

    fix a typo

--- test/cpp/news.cpp
+++ test/cpp/news.cpp
@@ -115,7 +115,7 @@ NAN_METHOD(NewScript) {
 
 NAN_METHOD(NewScript2) {
   v8::ScriptOrigin origin(
-#if NODE_MODULE_VERSION >= NODE_18_MODULE_VERSION
+#if NODE_MODULE_VERSION >= NODE_18_0_MODULE_VERSION
     info.GetIsolate(),
 #endif
     New<v8::String>("x").ToLocalChecked());
@@ -136,7 +136,7 @@ NAN_METHOD(CompileScript) {
 
 NAN_METHOD(CompileScript2) {
   v8::ScriptOrigin origin(
-#if NODE_MODULE_VERSION >= NODE_18_MODULE_VERSION
+#if NODE_MODULE_VERSION >= NODE_18_0_MODULE_VERSION
     info.GetIsolate(),
 #endif
     New<v8::String>("x").ToLocalChecked());
