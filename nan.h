4d1105e2f22feca3f464ddc4e895bb7b89bff244
Seth Westphal <westy92@gmail.com>
2023-04-24 22:11:37 -0500

    Node.js 20.

--- nan.h
+++ nan.h
@@ -13,7 +13,7 @@
  *
  * MIT License <https://github.com/nodejs/nan/blob/master/LICENSE.md>
  *
- * Version 2.17.0: current Node 19.1.0, Node 0.12: 0.12.18, Node 0.10: 0.10.48, iojs: 3.3.1
+ * Version 2.17.0: current Node 20.0.0, Node 0.12: 0.12.18, Node 0.10: 0.10.48, iojs: 3.3.1
  *
  * See https://github.com/nodejs/nan for the latest update to this file
  **********************************************************************************/
@@ -46,6 +46,7 @@
 #define NODE_17_0_MODULE_VERSION 102
 #define NODE_18_0_MODULE_VERSION 108
 #define NODE_19_0_MODULE_VERSION 111
+#define NODE_20_0_MODULE_VERSION 115
 
 #ifdef _MSC_VER
 # define NAN_HAS_CPLUSPLUS_11 (_MSC_VER >= 1800)
