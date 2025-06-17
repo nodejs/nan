f88eb2c35c47c12b4f2e3391429c23f5e0392816
agracio <agracio@users.noreply.github.com>
2025-06-17 16:47:32 +0100

    merging https://github.com/nodejs/nan/pull/979

--- test/js/methodswithdata-test.js
+++ test/js/methodswithdata-test.js
@@ -16,23 +16,26 @@ test('SetMethod with data', function (t) {
 });
 
 test('accessors with data', function (t) {
-    t.plan(7)
+    t.plan(9)
     var settergetter = bindings.create()
     t.equal(settergetter.prop1, 'this is property 1')
     t.ok(settergetter.prop2 === '')
     settergetter.prop2 = 'setting a value'
     t.equal(settergetter.prop2, 'setting a value')
     t.equal(settergetter.log(),
-      'New()\n' +
-      'Prop1:GETTER(this is property 1)\n' +
-      'Prop2:GETTER()\n' +
-      'Prop2:SETTER(setting a value)\n' +
-      'Prop2:GETTER(setting a value)\n'
+        'New()\n' +
+        'Prop1:GETTER(this is property 1)\n' +
+        'Prop2:GETTER()\n' +
+        'Prop2:SETTER(setting a value)\n' +
+        'Prop2:GETTER(setting a value)\n'
     )
     var derived = Object.create(settergetter)
     t.equal(derived.prop1, 'this is property 1')
     derived.prop2 = 'setting a new value'
     t.equal(derived.prop2, 'setting a new value')
-    t.equal(settergetter.prop2, 'setting a new value')
+    t.equal(settergetter.prop2, 'setting a value')
+    settergetter.prop2 = 'setting another value'
+    t.equal(settergetter.prop2, 'setting another value')
+    t.equal(derived.prop2, 'setting a new value')
   })
   
