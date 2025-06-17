fa3d61eb8bb4751f0068c694498707fe605cab02
agracio <agracio@users.noreply.github.com>
2025-06-17 17:25:55 +0100

    updating tests

--- test/js/weak-test.js
+++ test/js/weak-test.js
@@ -10,38 +10,46 @@ const test     = require('tap').test
     , testRoot = require('path').resolve(__dirname, '..')
     , bindings = require('bindings')({ module_root: testRoot, bindings: 'weak' })
     , gc = require('./gc-fn');
+const nodeVersion = parseInt(process.versions.node.split('.')[0]);
 
-test('weak', function (t) {
-  t.plan(3);
+// https://github.com/nodejs/nan/issues/995
+// https://chromium-review.googlesource.com/c/v8/v8/+/4733273
+// Week tests are incompatible with recent V8 changes
 
-  var weak = bindings, count = 0;
-  t.type(weak.hustle, 'function');
+if(nodeVersion < 24){
+  test('weak', function (t) {
+    t.plan(3);
 
-  weak.hustle(function () {}, function (val) {
-    t.equal(val, 42);
-    count++;
+    var weak = bindings, count = 0;
+    t.type(weak.hustle, 'function');
+
+    weak.hustle(function () {}, function (val) {
+      t.equal(val, 42);
+      count++;
+    });
+
+    var timeout = setTimeout(function () {
+      // run weak callback, should dispose
+      gc();
+
+      // do not run weak callback
+      gc();
+
+      if (count > 0) {
+        clearTimeout(timeout);
+        t.equal(count, 1);
+      }
+    }, 100);
   });
 
-  var timeout = setTimeout(function () {
-    // run weak callback, should dispose
-    gc();
+  test('weak external', function (t) {
+    t.plan(2);
 
-    // do not run weak callback
-    gc();
+    var weak = bindings;
+    t.type(weak.weakExternal, 'function');
 
-    if (count > 0) {
-      clearTimeout(timeout);
-      t.equal(count, 1);
-    }
-  }, 100);
-});
+    weak.weakExternal();
+    t.ok(true);  // All good if the previous line didn't crash.
+  });
+}
 
-test('weak external', function (t) {
-  t.plan(2);
-
-  var weak = bindings;
-  t.type(weak.weakExternal, 'function');
-
-  weak.weakExternal();
-  t.ok(true);  // All good if the previous line didn't crash.
-});
--- test/js/weak2-test.js
+++ test/js/weak2-test.js
@@ -10,28 +10,35 @@ const test     = require('tap').test
     , testRoot = require('path').resolve(__dirname, '..')
     , bindings = require('bindings')({ module_root: testRoot, bindings: 'weak2' })
     , gc = require('./gc-fn');
+const nodeVersion = parseInt(process.versions.node.split('.')[0]);
 
-test('weak2', function (t) {
-  t.plan(3);
+// https://github.com/nodejs/nan/issues/995
+// https://chromium-review.googlesource.com/c/v8/v8/+/4733273
+// Week tests are incompatible with recent V8 changes
 
-  var weak2 = bindings, count = 0;
-  t.type(weak2.hustle, 'function');
+if(nodeVersion < 24){
+  test('weak2', function (t) {
+    t.plan(3);
 
-  weak2.hustle(function (val) {
-    t.equal(val, 42);
-    count++;
+    var weak2 = bindings, count = 0;
+    t.type(weak2.hustle, 'function');
+
+    weak2.hustle(function (val) {
+      t.equal(val, 42);
+      count++;
+    });
+
+    // run weak callback, should dispose
+    gc();
+
+    // do not run weak callback
+    gc();
+
+    var timeout = setTimeout(function () {
+      if (count > 0) {
+        clearTimeout(timeout);
+        t.equal(count, 1);
+      }
+    }, 100);
   });
-
-  // run weak callback, should dispose
-  gc();
-
-  // do not run weak callback
-  gc();
-
-  var timeout = setTimeout(function () {
-    if (count > 0) {
-      clearTimeout(timeout);
-      t.equal(count, 1);
-    }
-  }, 100);
-});
+}
