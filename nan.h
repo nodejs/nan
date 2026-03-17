fcc7b7d4698f9cd87c7dc08b431ab82bed1b3b8c
Shawn Salekin <150074424+ssalekin-extron@users.noreply.github.com>
2026-03-17 10:18:57 -0400

    Add EmbedderDataTypeTag to SetAlignedPointerInInternalField and GetAlignedPointerFromInternalField for v8 >= 14 (#1010)
    * added data type tags to SetAlignedPointerInInternalField and GetAlignedPointerFromInternalField for v8 >= 14.2.194

--- nan.h
+++ nan.h
@@ -819,14 +819,31 @@ inline uv_loop_t* GetCurrentEventLoop() {
   inline void* GetInternalFieldPointer(
       v8::Local<v8::Object> object
     , int index) {
+#if (V8_MAJOR_VERSION > 14) || \
+    (V8_MAJOR_VERSION == 14 && V8_MINOR_VERSION > 2) || \
+    (V8_MAJOR_VERSION == 14 && V8_MINOR_VERSION == 2 && V8_BUILD_NUMBER >= 194)
+    return object->GetAlignedPointerFromInternalField(
+        index, v8::kEmbedderDataTypeTagDefault
+    );
+# else
     return object->GetAlignedPointerFromInternalField(index);
+# endif
   }
 
   inline void SetInternalFieldPointer(
       v8::Local<v8::Object> object
     , int index
     , void* value) {
+#if (V8_MAJOR_VERSION > 14) || \
+    (V8_MAJOR_VERSION == 14 && V8_MINOR_VERSION > 2) || \
+    (V8_MAJOR_VERSION == 14 && V8_MINOR_VERSION == 2 && V8_BUILD_NUMBER >= 194)
+    return object->GetAlignedPointerFromInternalField(
+    object->SetAlignedPointerInInternalField(
+        index, value, v8::kEmbedderDataTypeTagDefault
+    );
+# else
     object->SetAlignedPointerInInternalField(index, value);
+# endif
   }
 
 # define NAN_GC_CALLBACK(name)                                                 \
